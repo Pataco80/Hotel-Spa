106ac740e15644b21442bea1989f03cacbc64258
Pataco80 <pataco80@gmail.com>
2017-05-29 21:23:32 +0200

    Amélioration du menu, mise en place de la galerie, amélioration du header
    Amélioration du menu et des effets sous-menu avec "Retour" au lieu de
    "Back" et comportement
    
    Amélioration de l’apparence du bouton de menu sur mobile
    
    Amélioration de l’overlay
    
    Amélioration de la grille de Our Rooms pour un visuel confortable sur
    mobile en landscape et petites tablettes
    
    Mise en place de la galerie lightbox

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+
+
+$(function(){
+  $('#gallery a').on('click', function(){
+    var image = $(this).find('img').attr('src');
+    var imageselected = image.split('thumbnail');
+    var fullPath = imageselected[0] + 'fullsize' + imageselected[1];
+    $('#modal-gallery #image-container').attr('src', fullPath);
+  });
+});
+
 $(document).foundation();
